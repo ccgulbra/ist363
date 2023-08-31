28ee8efa85500cb65445f1f65a576af36faad7be
ccgulbra <92338979+ccgulbrandsen@users.noreply.github.com>
2023-08-31 17:56:39 -0400

    hi

---
+++ starbucks/js/main.js
@@ -0,0 +1,15 @@
+// console.log("js has been loaded");
+
+// variables, methods, and functions
+
+// 1. variables
+const myName = "Cc Gulbrandsen";
+console.log(myName); //if there are "quotes" you are refering to variable
+
+const myAge = 21;
+const myCity = "Syracuse";
+
+const introduction = `Hi, my name is ${myName}`; `I am ${myAge} years old and I live in ${myCity}`;
+
+
+console.log(introduction);
