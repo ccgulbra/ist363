59c406d4e50af26981f9c1122be15cc7b32f1c85
ccgulbra <92338979+ccgulbrandsen@users.noreply.github.com>
2023-09-05 15:36:22 -0400

    hi

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -9,7 +9,17 @@ console.log(myName); //if there are "quotes" you are refering to variable
 const myAge = 21;
 const myCity = "Syracuse";
 
-const introduction = `Hi, my name is ${myName}`; `I am ${myAge} years old and I live in ${myCity}`;
-
+const introduction = `Hi, my name is ${myName} I am ${myAge} years old and I live in ${myCity}`;
 
 console.log(introduction);
+
+// 2. methods
+const menuBtn = document.getElementById("menuBtn");
+const overlay = document.getElementById("overlay");
+console.log(menuBtn);
+
+// need to identify: "event name", callback function // every function has its own set of () and {}
+menuBtn.addEventListener("click", function() {
+    //console.log("menu button has been clicked");
+    overlay.classList.add("active");
+}); // end of menuBtn click event
