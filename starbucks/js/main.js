50e385e5fab786a39907db9dde74997ed02bf360
ccgulbra <92338979+ccgulbrandsen@users.noreply.github.com>
2023-09-12 18:19:20 -0400

    attendance 9/12/23. Finished flyout menu. Started coffee menu

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -4,14 +4,14 @@
 
 // 1. variables
 const myName = "Cc Gulbrandsen";
-console.log(myName); //if there are "quotes" you are refering to variable
+//console.log(myName); //if there are "quotes" you are refering to variable
 
 const myAge = 21;
 const myCity = "Syracuse";
 
 const introduction = `Hi, my name is ${myName}, I am ${myAge} years old and I live in ${myCity}.`;
 
-console.log(introduction);
+//console.log(introduction);
 
 // 2. methods
 const menuBtn = document.getElementById("menuBtn");
@@ -31,3 +31,31 @@ closeBtn.addEventListener("click", function() {
     overlay.classList.remove("active");
 }); //end of closeBtn click event
 
+// arrays and objects
+
+//basic variable
+
+const coffeeName = "Mocha"; //const prevents variable from being modified
+const coffeeHeadline = document.createElement ("h2");
+coffeeHeadline.textContent = coffeeName;
+document.body.appendChild(coffeeHeadline);
+
+// objects
+const coffee = {
+    name: "Mocha",
+    price: 3.99,
+    description: "This is a delicious cup of coffee."
+};
+//console.log(coffee.description);
+
+const coffeeObjHeadline = document.createElement("h2");
+coffeeObjHeadline.textContent = coffee.name;
+document.body.appendChild(coffeeObjHeadline);
+
+// arrays [0,1,2]
+const coffees = ["Mocha", "Latte", "Espresso"];
+//console.log(coffees[2]);
+
+coffees.forEach(function(coffee) {
+    console.log(coffee);
+});
